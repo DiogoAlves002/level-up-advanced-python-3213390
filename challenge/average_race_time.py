bc3e9964b54609768664522ff58c9b51a8f88caa
Diogo Alves <76565133+DiogoAlves002@users.noreply.github.com>
2024-01-28 23:47:33 +0000

    add avg race time and markdown challenges

--- challenge/average_race_time.py
+++ challenge/average_race_time.py
@@ -7,13 +7,35 @@ import datetime
 def get_data():
     """Return content from the 10k_racetimes.txt file"""
     with open('10k_racetimes.txt', 'rt') as file:
-        content = file.read()
+        content = file.readlines()
     return content
 
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
+    jen = "Jennifer Rhines"
+    jen_times = []
     races = get_data()
-    pass
+    header = races[0]
+    race_time_i = header.find("TIME")
+    athlete_i = header.find("Athlete")
+    date_i = header.find("Race date")
+    #date_of_birth_i = header.find("Date of birth")
+    #location_i = header.find("Location")
+
+    for race in races[1:]:
+        
+        race_time = race[race_time_i: athlete_i].strip()
+        athlete = race[athlete_i: date_i].strip()
+        #date = race[date_i: date_of_birth_i].strip()
+        #date_of_birth = race[date_of_birth_i: location_i].strip()
+        #location = race[location_i:].strip()
+        
+        if jen not in athlete:
+            continue
+        jen_times.append(race_time)
+
+
+    return jen_times
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
@@ -21,5 +43,21 @@ def get_average():
        m corresponds to a minutes digit
        s corresponds to a seconds digit
        M corresponds to a milliseconds digit (no rounding, just the single digit)"""
+    
     racetimes = get_rhines_times()
-    pass
+
+    total = datetime.timedelta()
+    for race_t in racetimes:
+        m_s_M = re.split(r":|\.", race_t)
+        print(m_s_M)
+
+        m = int(m_s_M[0])
+        s = int(m_s_M[1])
+        M = 0
+        if len(m_s_M) == 3:
+            M = int(m_s_M[2])
+        
+        total += datetime.timedelta(minutes= m, seconds= s, milliseconds= M)
+    
+    return f'{total/len(racetimes)}'[2:-5]
+    
--- challenge/html2markdown.py
+++ challenge/html2markdown.py
@@ -1,4 +1,42 @@
 
 def html2markdown(html):
     '''Take in html text as input and return markdown'''
-    pass
+    
+    # italics
+    html = html.replace("<em>", "*")
+    html = html.replace("</em>", "*")
+
+    # white spaces
+    html = " ".join(html.split())
+
+    # paragraphs
+    while "</p>" in html:
+        end_of_p = html.find("</p>")
+        if html[end_of_p + 4: end_of_p + 4 + 3] == "<p>": # consecutive paragraphs -> \n\n
+            print("yo")
+            html = html.replace("<p>", "", 2)
+            html = html.replace("</p>", "\n\n", 1)
+            html = html.replace("</p>", "", 1)
+        else:
+            html = html.replace("<p>", "")
+            html = html.replace("</p>", "")
+
+    # urls
+    while "<a" in html:
+        a_pos = html.find("<a")
+        before_a = html[: a_pos]
+
+        href_pos = html.find("\"", a_pos) + 1
+        href_end = html.find("\"", href_pos + 1)
+        href = html[href_pos : href_end]
+
+        link_pos = html.find(">", href_end) + 1
+        link_end = html.find("</", link_pos)
+        link = html[link_pos : link_end]
+
+        a_end = html.find(">", link_end) + 1
+        after_a = html[a_end:]
+
+        html = before_a + f'[{link}]({href})' + after_a
+
+    return html
