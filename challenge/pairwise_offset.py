846dd800e1aed11d85d2d172b7528a36361306d9
Diogo Alves <76565133+DiogoAlves002@users.noreply.github.com>
2024-01-29 22:13:38 +0000

    add pairwise_offset

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,37 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offsetV2(sequence, fillvalue= "*", offset= 0):
+  if not offset:
+    return [(a, a) for a in sequence]
+  
+  it = []
+
+  for i in range(offset):
+    if i < len(sequence):
+        it.append((sequence[i], fillvalue))
+        continue
+    it.append((fillvalue, fillvalue))
+  
+  count = 0
+  if offset < len(sequence):
+    for e in range(offset, len(sequence)):
+      it.append((sequence[e], sequence[count]))
+      count += 1
+  for r in range(count, len(sequence)):
+    it.append((fillvalue, sequence[r]))
+
+  return it
+
+
+
+def pairwise_offset(sequence, fillvalue= "*", offset= 0):
+  it = []
+
+  seq = [fillvalue] * offset + list(sequence)
+  for i in range(len(seq)):
+    if i < len(sequence):
+      pair = (sequence[i], seq[i])
+    else:
+      pair = (fillvalue, seq[i])
+    
+    it.append(pair)
+
+  return it
